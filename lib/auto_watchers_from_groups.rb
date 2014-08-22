0f4b73d16429f9914d550104ad2b5fc3e3bb8b1d
Alexander Kuznecov <alexander@kuznetcov.me>
2014-08-23 04:27:49 +0700

    allows to add watchers when category default assignee is picked #1

--- lib/auto_watchers_from_groups.rb
+++ lib/auto_watchers_from_groups.rb
@@ -11,16 +11,34 @@ module AutoWatchersFromGroups
 		end
 
 		def auto_watchers(context)
+
 			@settings ||= Setting.plugin_redmine_auto_watchers_from_groups
+			@issue = Issue.find context[:issue]
+
 			if context[:params][:issue]
+
+				# normal group assign
 				if @settings['groups_enabled'].include? context[:params][:issue][:assigned_to_id]
-					@issue = Issue.find context[:issue]
-					@group = Group.find context[:params][:issue][:assigned_to_id]
-					@group.users.each do |new_watcher|
+					group = Group.find context[:params][:issue][:assigned_to_id]
+				end
+
+				# group assign with category default assignee
+				@project = context[:params][:project_id]
+				@project = context[:params][:issue][:project_id] if context[:params][:issue][:project_id]
+				if context[:params][:issue][:assigned_to_id].blank? && !context[:params][:issue][:category_id].blank? && context[:params][:Commit] == "Create"
+					if @settings['groups_enabled'].include? group_id = Project.find(@project).issue_categories.find_by_id(context[:params][:issue][:category_id]).assigned_to_id.to_s 
+						group = Group.find group_id
+					end
+				end
+				
+				unless group.nil?
+					group.users.each do |new_watcher|
 						Watcher.create(:watchable => @issue, :user => new_watcher)
 					end
 				end
+
 			end
+
 		end
 
 		alias_method :controller_issues_bulk_edit_before_save, :controller_issues_edit_before_save
